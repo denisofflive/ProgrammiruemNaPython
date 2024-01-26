85e193b86c3f384650bdd9a3ed7b6e6fb77b0a9e
denis <denisofflive@mail.ru>
2024-01-26 21:52:23 +0300

    craps_roller

--- CHAPTER3/craps_roller.py
+++ CHAPTER3/craps_roller.py
@@ -14,6 +14,6 @@ die2 = random.randint(1, 6)
 
 total = die1 + die2
 
-print("Пpи вашем броске выпало", die1, "и", die2, "очков. в сумме", total)
+print("Пpи вашем броске выпало", die1, "и", die2, "очков. В сумме", total)
 
 input("\n\nНажмите Enter, чтобы выйти.")
