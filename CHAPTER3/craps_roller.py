8e8972cd026f43f866da608d1169e2e20c63f4ec
denis <denisofflive@mail.ru>
2024-01-26 21:51:59 +0300

    craps_roller

--- CHAPTER3/craps_roller.py
+++ CHAPTER3/craps_roller.py
@@ -5,7 +5,12 @@ import random
 
 # Создаем случайные числа из диапазона 1 - 6
 die1 = random.randint(1, 6)
-die2 = random.randrange(6) + 1
+die2 = random.randint(1, 6)
+
+# """Второй способ - можно использовать функцию randrange,
+# но в ней отсчёт идёт с нуля, поэтому добавляем +1 и начало идёт с 1"""
+# die1 = random.randrange(6) + 1
+# die2 = random.randrange(6) + 1
 
 total = die1 + die2
 
