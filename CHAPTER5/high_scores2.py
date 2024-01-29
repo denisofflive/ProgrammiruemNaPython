eaffe124a8c72744d9fd70afeb94e6a250ae0a71
denis <denisofflive@mail.ru>
2024-01-29 18:48:23 +0300

    high_scores2

---
+++ CHAPTER5/high_scores2.py
@@ -0,0 +1,47 @@
+# Рекорды 2.0
+#  Демонстрирует вложенные последовательности
+
+scores = []
+
+choice = None
+while choice != "0":
+
+    print(
+        """
+    Рекорды 2. О 
+    О - Выйти 
+    1 - Показать рекорды 
+    2 - Добавить рекорд 
+        """
+    )
+
+    choice = input("Baш выбор: ")
+    print()
+
+    # выход
+    if choice == "0":
+        print("До свидания.")
+
+    # вь1вод таблицы рекордов
+    elif choice == "1":
+        print("Рекорды\n")
+        print("ИМЯ\tРЕЗУЛЬТАТ")
+        for entry in scores:
+            score, name = entry
+            print(name, "\t", score)
+
+    # add a score
+    elif choice == "2":
+        name = input("Впишите имя игрока:: ")
+        score = int(input("Bnишитe его результат: "))
+        entry = (score, name)
+        scores.append(entry)
+        scores.sort(reverse=True)
+        scores = scores[:5]  # в списке остается только 5 рекордов
+
+
+    # непонятный пользовательский ввод
+    else:
+        print("Извините, в меню нет пункта", choice)
+
+input("\n\nНажмите Enter, чтобы выйти.")
